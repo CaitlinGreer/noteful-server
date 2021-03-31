2ee4382acf43685085b9d7de021d2955fe5d4450
CaitlinGreer <clento1121@gmail.com>
2021-03-31 10:29:26 -0400

    created makeNotes/FoldersArray fixtures

---
+++ test/notes-fixtures.js
@@ -0,0 +1,27 @@
+function makeNotesArray() {
+    return [
+        {
+            id: 1,
+            note_name: 'Note Number 1',
+            note_content: 'Content for note number 1',
+            date_modified: '2021-01-22T16:28:32.615Z',
+            folder_id: 1
+        },
+        {
+            id: 2,
+            note_name: 'Note Number 2',
+            note_content: 'Content for note number 2',
+            date_modified: '2021-01-22T6:28:32.615Z',
+            folder_id: 2
+        },
+        {
+            id: 3,
+            note_name: 'Note Number 3',
+            note_content: 'Content for note number 3',
+            date_modified: '2021-01-22T16:28:32.615Z',
+            folder_id: 3
+        },
+    ]
+}
+
+module.exports = makeNotesArray
