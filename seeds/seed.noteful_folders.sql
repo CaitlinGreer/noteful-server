1aa09f373c8759c86db4c0b903c4dabbd3047170
CaitlinGreer <clento1121@gmail.com>
2021-04-01 09:03:25 -0400

    seeded the db

---
+++ seeds/seed.noteful_folders.sql
@@ -0,0 +1,5 @@
+INSERT INTO noteful_folders (folder_name)
+VALUES
+  ('To-Dos'),
+  ('School Notes'),
+  ('Not Important');
---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,8 @@
+INSERT INTO noteful_notes (note_name, note_content, modified, folder_id)
+VALUES
+ ('Make drs appointment', 'Need to do checkup', '2021-01-05', 1),
+ ('Take out trash', 'Put the chair on the curb', '2021-03-22', 1),
+ ('Get Floor Cleaner', 'tackle all the grout in tiles', '2021-03-20', 1),
+ ('Node', 'Find outside resources to learn from', '2021-03-05', 2),
+ ('React Router', 'INSERT NOTES', '2021-02-22', 2),
+ ('Movies to watch', 'BTTF, Indiana Jones, Ghostbusters', '2021-01-20', 3);
